cef9883625d902306cf4af971dbaa3707c29b85f
orneysoriginal <134702435+OrneysOriginal@users.noreply.github.com>
2025-05-01 12:58:59 +0300

    Update database.py

--- src/database.py
+++ src/database.py
@@ -15,16 +15,6 @@ class Base(DeclarativeBase):
     pass
 
 
-async def create_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-
-
-async def delete_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-
-
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
